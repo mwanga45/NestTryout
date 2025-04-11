c69a0dbd06949af41c01463dd6cdfd16c79c0016
Lynxprazo <issamwanga02@gmail.com>
2025-04-11 22:46:09 +0300

    more changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    findAll(){
+       return []
+    }
+    @Get(':id')
+    findOne(@Param('id') id: string){
+      return{id}
+    }
+}
