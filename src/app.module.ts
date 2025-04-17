1de4b1655ccb2372fd3a2567e23d7105a71f5e86
Lynxprazo <issamwanga02@gmail.com>
2025-04-17 14:06:52 +0300

    more changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { WorkersModule } from './workers/workers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, WorkersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/workers/workers.service.ts
@@ -0,0 +1,32 @@
+// import { Injectable } from '@nestjs/common';
+// export type Users = any
+// @Injectable()
+// export class WorkersService {
+//          private readonly user=[
+//             {
+//                 username:"Isack",
+//                 password:"1235"
+//             },
+//             {
+//                 username:"Ramadhani",
+//                 password:"54321"
+//             },
+//             {
+//                 username:"James",
+//                 password:"7890"
+//             },
+//             {
+//                 username:"Mengiseni",
+//                 password:"135"
+//             },
+//             {
+//                 username:"Minja",
+//                 password:"5678"
+//             },
+//         ]
+
+//         async findone(username:string) :Promise<Users|undefined>{
+//             return  this.user.find(user => user.username === username)
+//         }
+    
+// }
