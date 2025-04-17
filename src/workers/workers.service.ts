7b898614b7af7efa091ffa2cbfb1e0ca42b09d06
Lynxprazo <issamwanga02@gmail.com>
2025-04-17 14:15:52 +0300

    more changes

--- src/workers/workers.service.ts
+++ src/workers/workers.service.ts
@@ -30,3 +30,35 @@
 //         }
     
 // }
+
+import  {Injectable} from "@nestjs/common"
+export type Users  = any
+
+@Injectable()
+export class WorkersService{
+    private readonly User  =[
+        {
+            username:"isack",
+            password:"123"
+        },
+        {
+            username:"Samweli",
+            password:"345"
+        },
+        {
+            username:"Godwin",
+            password:"321"
+        },
+        {
+            username:"Mwacha",
+            password:"312"
+        },
+        {
+            username:"Minja",
+            password:"567"
+        },
+    ];
+    async findOne(username : string):Promise<Users|undefined>{
+        return this.User.find(user => user.username === username)
+    } 
+}
